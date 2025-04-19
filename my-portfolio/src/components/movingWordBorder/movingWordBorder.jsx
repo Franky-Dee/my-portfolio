a536cf638e22b7b084837b6c0c45495f90d89fb8
franco <franco@taskflow.co.za>
2025-04-19 11:35:44 +0200

    [IMP] movingWordBorder: Made styling changes and fixes to allow to function properly

--- my-portfolio/src/components/movingWordBorder/movingWordBorder.jsx
+++ my-portfolio/src/components/movingWordBorder/movingWordBorder.jsx
@@ -1,31 +1,50 @@
 import './movingWordBorder.css';
 
-const words = ["React", "Creativity", "Border", "Scroll", "Design", "Code", "Innovate"];
-const fonts = ["'Courier New', monospace", "'Times New Roman', serif", "'Arial', sans-serif", "'Comic Sans MS', cursive"];
+const words = ["Franky", "Franky", "Franky", "Franky", "Franky", "Franky", "Franky"];
+const fonts = [
+  "'Courier New', monospace",
+  "'Times New Roman', serif",
+  "'Arial', sans-serif",
+  "'Comic Sans MS', cursive"
+];
 
 function MovingWordBorder() {
   return (
     <>
       <BorderSide position="top" />
       <BorderSide position="bottom" />
-      <BorderSide position="left" vertical />
-      <BorderSide position="right" vertical />
+      <BorderSide position="left" />
+      <BorderSide position="right" />
     </>
   );
 }
 
-function BorderSide({ position, vertical = false }) {
+function BorderSide({ position }) {
   return (
     <div className={`moving-text-border ${position}-border`}>
       <div className="moving-text">
-        {Array.from({ length: 50 }, (_, i) => (
-          <span
-            key={i}
-            style={{ fontFamily: fonts[i % fonts.length], margin: "0 10px" }}
-          >
-            {words[i % words.length]}
-          </span>
-        ))}
+        {[...Array(2)].flatMap((_, j) =>
+          Array.from({ length: 50 }, (_, i) => (
+            <span
+              key={`${j}-${i}`}
+              style={{
+                fontSize: "50px",
+                fontWeight: "bold",
+                fontFamily: fonts[i % fonts.length],
+                margin: "100px 100px",
+                display: "inline-block",
+                transform:
+                  position === "left"
+                    ? "rotate(90deg)"
+                    : position === "right"
+                    ? "rotate(-90deg)"
+                    : "none"
+              }}
+            >
+              {words[i % words.length]}
+            </span>
+          ))
+        )}
       </div>
     </div>
   );
