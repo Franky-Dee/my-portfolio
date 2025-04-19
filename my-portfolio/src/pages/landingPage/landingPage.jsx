507020ba19222bf4b310ddccb2bfcd7eae605c44
franco <franco@taskflow.co.za>
2025-04-19 10:56:20 +0200

    [REF] Changed project structure to be able to begin with landing page

---
+++ my-portfolio/src/pages/landingPage/landingPage.jsx
@@ -0,0 +1,13 @@
+import MovingWordBorder from '../../components/movingWordBorder/movingWordBorder.jsx'
+
+function LandingPage() {
+
+  return (
+    <>
+        <MovingWordBorder>
+        </MovingWordBorder>
+    </>
+  )
+}
+
+export default LandingPage
