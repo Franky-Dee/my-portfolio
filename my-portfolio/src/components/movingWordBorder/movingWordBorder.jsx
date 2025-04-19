e7c02f6568d943e2fafd39f4056f08de1460482d
franco <franco@taskflow.co.za>
2025-04-19 11:08:34 +0200

    [ADD] movingWordBorder: Added intial code for moving text border

--- my-portfolio/src/components/movingWordBorder/movingWordBorder.jsx
+++ my-portfolio/src/components/movingWordBorder/movingWordBorder.jsx
@@ -1,9 +1,34 @@
-function MovingWordBorder() {
+import './movingWordBorder.css';
 
+const words = ["React", "Creativity", "Border", "Scroll", "Design", "Code", "Innovate"];
+const fonts = ["'Courier New', monospace", "'Times New Roman', serif", "'Arial', sans-serif", "'Comic Sans MS', cursive"];
+
+function MovingWordBorder() {
   return (
     <>
+      <BorderSide position="top" />
+      <BorderSide position="bottom" />
+      <BorderSide position="left" vertical />
+      <BorderSide position="right" vertical />
     </>
-  )
+  );
 }
 
-export default MovingWordBorder
+function BorderSide({ position, vertical = false }) {
+  return (
+    <div className={`moving-text-border ${position}-border`}>
+      <div className="moving-text">
+        {Array.from({ length: 50 }, (_, i) => (
+          <span
+            key={i}
+            style={{ fontFamily: fonts[i % fonts.length], margin: "0 10px" }}
+          >
+            {words[i % words.length]}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MovingWordBorder;
