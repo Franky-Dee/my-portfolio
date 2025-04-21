b4636f2612e9e03ae87a7ef7522e46233ca9d9d6
franco <franco@taskflow.co.za>
2025-04-21 17:26:04 +0200

    [IMP] titleBlock: Made font different for each reload

--- my-portfolio/src/components/titleBlock/titleBlock.jsx
+++ my-portfolio/src/components/titleBlock/titleBlock.jsx
@@ -1,10 +1,28 @@
-import "./titleBlock.css"
+import  {useState, useEffect} from "react";
+import "./titleBlock.css";
+
+const fonts = [
+    "'DM Serif Text', serif",
+    "'Imperial Script', cursive",
+    "'Fascinate Inline', system-ui",
+    "'Silkscreen', sans-serif",
+    "'Creepster', system-ui",
+    "'Notable', sans-serif",
+    "'Libre Barcode 128 Text', system-ui",
+    "'Danfo', serif",
+];
 
 function TitleBlock() {
+  const [font, setFont] = useState(fonts[0])
+
+  useEffect(() => {
+    const randomFont = fonts[Math.floor(Math.random() * fonts.length)]
+    setFont(randomFont)
+  })
 
   return (
     <>
-        <div className="name">
+        <div className="name" style={{fontFamily: font}}>
             FrankyDee
         </div>
     </>
